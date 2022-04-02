85e1610adf78e5c1e7429e79631a652af991568a
Ggmarg <gevorg.margaryan.900@gmail.com>
2022-04-02 16:12:00 +0400

    small

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 let menu = document.querySelector('#menu');
 let nav = document.querySelector('.navigation');
-let content = document.querySelectorAll('.content a');
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
